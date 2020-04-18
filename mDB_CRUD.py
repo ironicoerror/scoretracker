c249d573ca71841db680a4f5b984b019315f7a6d
ironicoerror <rogalla.tobias@googlemail.com>
2020-04-18 23:30:47 +0200

    code refactoring

--- mDB_CRUD.py
+++ mDB_CRUD.py
@@ -13,34 +13,32 @@ from bson.objectid import ObjectId
 LICENSE_STRING = None 
 CLIENT = None
 DB = None
+
 def set_credentials(license_file):
+    """copies credential string from file given"""
     return ImportFromFile(license_file).import_direct()
 
-def db_init(db_name):
-    """database initiation"""
-    if LICENSE_STRING != None:
-        client = pymongo.MongoClient(LICENSE_STRING)
-        database = client[db_name]
-        return client, database
-    else:
+def connect_client():
+    """sets up a client conncetion to the database"""
+    if LICENSE_STRING == None:
         raise Exception("No License information given. Edit LICENSE_STRING.")
+    else:
+        return pymongo.MongoClient(LICENSE_STRING)
+
+def set_db(db_name):
+    """selects the database from mongodb"""
+    return CLIENT[db_name]
 
 def get_serverstatus():
     """returns the Server Status of the mongo DB Client"""
-    try:
-        DB.command("serverstatus")
-    except:
-        return 1 
-    return 0 
+    return DB.command("serverstatus")
 
 def create_data(upload_object, collection):
     """writes one dictionary object to the collection specified"""
-    if type(upload_object) == dict:
-        response = DB[collection].insert_one(upload_object)
-        return response
+    if type(upload_object) != dict:
+        raise TypeError("Data has to be dict type.")
     else:
-        print("Your data has to be of dict type.") 
-        raise TypeError
+        return DB[collection].insert_one(upload_object)
 
 def read_table(collection):
     """reads the whole collection table specified"""
@@ -48,87 +46,45 @@ def read_table(collection):
 
 def read_item(collection, unique_id):
     """reads an item in the collection from its ID and returns it as a dict"""
-    response = DB[collection].find_one({"_id" : ObjectId(unique_id)})
-    return response
+    return DB[collection].find_one({"_id" : ObjectId(unique_id)})
 
 def read_matchup(collection, field, searchstring):
     """reads all items in the collection that match a certain string and returns it as a dict"""
-    if field == "_id":
-        searchstring = ObjectId(searchstring)
-    response = DB[collection].find_one({field : ObjectId(unique_id)})
-    return response
+    if field == "_id": searchstring = ObjectId(searchstring)
+    return DB[collection].find_one({field : ObjectId(unique_id)})
 
 def update_data(update_object, collection):
     """searches for an entryid in the specified collection and updates it"""
-    response = DB[collection].update_one(
-            {"_id": update_object["_id"]}, {"$set": update_object})
-    return response
+    return DB[collection].update_one({"_id": update_object["_id"]}, {"$set": update_object})
 
 def delete_data(del_object, collection):
     """searches for an entryid and deletes the entry"""
-    response = DB[collection].delete_one({"_id": del_object["_id"]})
-    return response
-
-def create_sample_data(collection):
-    """writes a specified num of datapoints as sample to collection specified"""
-    game_table = read_table("games")
-    games = []
-    for game in game_table:
-        games.append(game["name"])
-    player_table = read_table("people")
-    ids = []
-    for player in player_table:
-        ids.append(player["_id"])
-    for x in range(0,10):
-        test_object ={
-                    'creation_date' : datetime.now(),
-                    'game' : choice(games),
-                    'team1' : choices(ids, k=2),
-                    'team2' : choices(ids, k=2),
-                    'score_team1' : randint(1,10),
-                    'score_team2' : randint(1,10)
-        }
-        last_response = create_data(test_object, collection)
-    return 0
+    return DB[collection].delete_one({"_id": del_object["_id"]})
 
 if __name__ == "__main__":
     usage_desc = """Usage:
-    {0} <lic_file> <list> to list the present dbs.
-    {0} <lic_file> <db_name> <status> to print the server status.
-    {0} <lic_file> <db_name> <list> to list the tables.
-    {0} <lic_file> <db_name> <read> <table> to read a tables content""".format(argv[0])
-    if len(argv) > 2:
+    {0} <lic_file> <list> to list the databases on the server.
+    {0} <lic_file> <list> <db_name> to list the tables.
+    {0} <lic_file> <list> <db_name> <table> to list a tables content.
+    {0} <lic_file> <status> <db_name> to print the database status.
+    """.format(argv[0])
+    commands = ["list", "status"]
+    if (len(argv) > 2) and (argv[2] in commands):
         LICENSE_STRING = ImportFromFile(argv[1]).import_direct()
-        CLIENT, DB = db_init(argv[2])
-        if len(argv) == 3:
-            if argv[2] == "list":
-                for database in CLIENT.list_databases():
-                    print(database["name"])
-        elif len(argv) > 3:
-            if len(argv) == 4:
-                if argv[3] == "status" or "s":
-                    print(get_serverstatus())
-                elif argv[3] == "list" or "l":
-                    for table in DB.list_collections():
-                        print(table["name"])
+        CLIENT = connect_client()
+        #list
+        if argv[2] == "list":
+            if len(argv) == 3:
+                print([database["name"] for database in CLIENT.list_databases()])
+            elif len(argv) == 4:
+                DB = set_db(argv[3])
+                print([table["name"] for table in DB.list_collections()])
             elif len(argv) == 5:
-                if argv[3] == "read" or "r":
-                    #pretty print of the specified table
-                    data = read_table(argv[4])
-                    if data.count() != 0:
-                        i = 0
-                        for entry in data:
-                            if i == 0:
-                                for key, value in entry.items():
-                                    print(key, " " * (len(str(value))-len(str(key))), end="\t") 
-                                    i += 1
-                                print("\n")
-                            for key, value in entry.items():
-                                print(value, end="\t")
-                            print("\n")
-                    else:
-                        raise Exception("Table not found. Check spelling. Valid Tables are:\n{0}".format([table["name"] for table in  DB.list_collections()]))
-        else:
-            print(usage_desc)
+                DB = set_db(argv[3])
+                print([doc for doc in read_table(argv[4])])
+        #status
+        if argv[2] == "status":
+            DB = set_db(argv[3])
+            print("Connected") if not get_serverstatus() else print("No connection")
     else:
         print(usage_desc) 
